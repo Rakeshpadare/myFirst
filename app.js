a9074bfd52d1a298538eb55a419defbc59fd8143
Rakeshpadare <rakeshpadare07@gmail.com>
2022-11-24 16:54:15 +0530

    elementsBYID

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log('My first code with github')
+// //  EXAMINE THE DOCUMENT OBJECTS ie DOM
+
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL)
+// console.log(document.title)
+// console.log(document.doctype)
+// // console.log(document.head)
+// // console.log(document.all)
+// // console.log(document.all[10])
+// // document.all[10].textContent='rakesh'
+// // console.log(document.forms)
+// // console.log(document.links)
+
+
+
+//------------------------------------------------------//
+
+// // get Element BY Class Name
+// var items=document.getElementsByClassName('list-group-item');
+// items[2].textContent='new changes';
+// items[2].style.fontWeight='bold';
+// items[1].style.backgroundColor='green'
+
+// // items.style.fontWeight='bold'----> this gives error to solve this problem we used loop
+// // get Elements bY class name
+// for(var i=1; i<items.length;i++){
+//     items[i].style.color='green'
+//     items[i].style.fontWeight='bold'
+// }
+
+// Get Elements by Tagname
+
+var li=document.getElementsByTagName('li');
+li[2].textContent='new changes';
+li[2].style.fontWeight='bold';
+li[1].style.backgroundColor='red'
---
+++ element.js
@@ -0,0 +1,12 @@
+// // GETELEMENTS BY ID//
+// console.log(document.getElementById('header-title'))
+// using variable
+
+var headerTitle=document.getElementById('header-title');
+// for header change bottom line 
+var headerTitle=document.getElementById('main-header');
+// console.log(headerTitle)
+// // headerTitle.textContent='Hi this is edited'
+// headerTitle.innerText='Nice again span '
+// console.log(headerTitle.innerText)
+headerTitle.style.borderBottom='solid 6px #000'
